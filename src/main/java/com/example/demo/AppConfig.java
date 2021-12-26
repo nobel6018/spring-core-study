626b0e6fc0776eb3ab7e483259cded14cbcaef97
Lee,YoungHoon <nobel6018@gmail.com>
2021-12-26 17:42:28 +0900

    feat: app config. 외부에서 DI 주입

---
+++ src/main/java/com/example/demo/AppConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import com.example.demo.discount.FixDiscountPolicy;
+import com.example.demo.member.MemberService;
+import com.example.demo.member.MemberServiceImpl;
+import com.example.demo.member.MemoryMemberRepository;
+import com.example.demo.order.OrderService;
+import com.example.demo.order.OrderServiceImpl;
+
+// 생성자 주입 방식
+public class AppConfig {
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+}
